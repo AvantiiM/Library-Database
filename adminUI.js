3574459ef9dc5169534572e77a23129544f8fdce
sk1nnyking06 <laidaniel06@gmail.com>
2023-03-22 11:58:47 -0500

    hi

--- adminUI.js
+++ adminUI.js
@@ -1,8 +1,63 @@
 <html>
-  <head>
-    <title>Admin Page</title>
-  </head>
-  <body>
-    <h1>ADMIN PAGE</h1>
-  </body>
+<head>
+	<title>My Tabs</title>
+	<style>
+		.tab {
+			background-color: orange;
+			color: white;
+			display: inline-block;
+			padding: 10px 20px;
+			cursor: pointer;
+			border-radius: 5px 5px 0 0;
+			margin-right: 5px;
+		}
+
+		.tab.active {
+			background-color: white;
+			color: orange;
+		}
+
+		.tabcontent {
+			display: none;
+			padding: 20px;
+			border: 1px solid orange;
+			border-top: none;
+		}
+
+		.tabcontent.active {
+			display: block;
+		}
+	</style>
+</head>
+<body>
+	<div class="tabs">
+		<div class="tab active" onclick="openTab('datamanagement')">Data Management</div>
+		<div class="tab" onclick="openTab('datareports')">Data Reports</div>
+	</div>
+	<div class="tabcontent active" id="datamanagement">
+		<h2>Data Management Content Goes Here</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id mauris sed libero pharetra finibus in eget sapien. Donec tincidunt, nibh a eleifend fringilla, purus dolor facilisis est, eget dignissim eros mi eu urna.</p>
+	</div>
+	<div class="tabcontent" id="datareports">
+		<h2>Data Reports Content Goes Here</h2>
+		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+	</div>
+
+	<script>
+		function openTab(tabName) {
+			var i, tabcontent, tablinks;
+			tabcontent = document.getElementsByClassName("tabcontent");
+			for (i = 0; i < tabcontent.length; i++) {
+				tabcontent[i].classList.remove("active");
+			}
+			tablinks = document.getElementsByClassName("tab");
+			for (i = 0; i < tablinks.length; i++) {
+				tablinks[i].classList.remove("active");
+			}
+			document.getElementById(tabName).classList.add("active");
+			event.currentTarget.classList.add("active");
+		}
+	</script>
+</body>
 </html>
+
--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
 var handle = {};
-handle["/"] = requestHandlers.login;
-handle["/login"] = requestHandlers.login;
-handle["/loginverify"] = requestHandlers.loginverify;
-handle["/search"] = requestHandlers.search;
+handle["/"] = requestHandlers.login; //http://localhost:3000/login
+handle["/login"] = requestHandlers.login; //http://localhost:3000/login
+handle["/loginverify"] = requestHandlers.loginverify; //not an actual page
+handle["/search"] = requestHandlers.search; //http://localhost:3000/search
 //handle["/searchresults"] = requestHandlers.searchresults;
-handle["/createUser"] = requestHandlers.createUser;
-handle["/addLogin"] = requestHandlers.addLogin;
-handle["/adminUI"] = requestHandlers.adminUI;
+handle["/createUser"] = requestHandlers.createUser; //http://localhost:3000/createUser
+handle["/addLogin"] = requestHandlers.addLogin; //not an actual page
+handle["/adminUI"] = requestHandlers.adminUI; //http://localhost:3000/adminUI
 
 
 server.start(router.route, handle);
