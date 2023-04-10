c1f8a9fbddc98d94ede532f0c22fbcc3397a3724
TheMordecai <33071553+TheMordecai@users.noreply.github.com>
2023-04-10 01:26:37 -0500

    Multiple pages changed
    portkill.js added that will kill the port currently active. Simply run node killport.js. Login changes made - not complete.

--- db_connect.js
+++ db_connect.js
@@ -14,7 +14,7 @@ var conn = new sql.ConnectionPool(config);
 
 sql.connect(config)
     .then(() => console.log('Connected to database'))
-    .catch(err => console.error('Failed to connect to database', err)); 
+    .catch(err => console.error('Failed to connect to database', err));
 
 //exports.conn = conn;
 exports.config = config;
