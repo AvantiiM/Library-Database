23cfd65b2a7776c52f04e83f917ba743153e0066
TheMordecai <33071553+TheMordecai@users.noreply.github.com>
2023-04-19 18:15:22 -0500

    Profile and getInfo() function changes

--- requestHandlers.js
+++ requestHandlers.js
@@ -119,112 +119,91 @@ function sendEJSFile(response, filename, msgtxt) {
 }
 
 function getInfo(response, postData, sessionData){
-    req.input('userId', sql.NVarChar, sessionData.logginId);
+    // req.input('userId', sql.NVarChar, sessionData.logginId);
+    let userId = sessionData.logginId;
 
-    let firstN;
-    let lastN;
-    let middleN;
-    let fullName;
-    let email;
-    let dep;
-    let balance;
-    console.log(sessionData.logginId)
-    sql.connect(config).then(function() {
-        return sessionData.logginId;
-      var username = sessionData.logginId;
-      var req = new sql.Request();
-      req.query("SELECT Faculty_ID, StudentID, GuestID FROM Login WHERE Username=" + '\'' + username + '\'', function (result, recordset) {
+    // perform any database queries or other operations as needed...
+
+    // send the response back to the client
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<p>User ID: " + userId + "</p>");
+    response.end();
+    // let firstN;
+    // let lastN;
+    // let middleN;
+    // let fullName;
+    // let email;
+    // let dep;
+    // let balance;
+    // console.log(sessionData.logginId)
+    // sql.connect(config).then(function() {
+    //     return sessionData.logginId;
+    //   var username = sessionData.logginId;
+    //   var req = new sql.Request();
+    //   req.query("SELECT Faculty_ID, StudentID, GuestID FROM Login WHERE Username=" + '\'' + username + '\'', function (result, recordset) {
         
-        if (recordset.recordsets[0].length > 0) { 
-          var faculty = recordset.recordsets[0][0].Faculty_ID;
-          var stud = recordset.recordsets[0][0].StudentID;
-          var guest = recordset.recordsets[0][0].GuestID;
-        }
-        if(faculty != null){
-            console.log("This is the Faculty ID called: " + faculty)
-            return faculty;
-            req.query("SELECT FirstN, LastN, MiddleN, Email, Department, Balance FROM Faculty WHERE Faculty_ID=" + '\'' + faculty + '\'', function (result, recordset) {
+    //     if (recordset.recordsets[0].length > 0) { 
+    //       var faculty = recordset.recordsets[0][0].Faculty_ID;
+    //       var stud = recordset.recordsets[0][0].StudentID;
+    //       var guest = recordset.recordsets[0][0].GuestID;
+    //     }
+    //     if(faculty != null){
+    //         console.log("This is the Faculty ID called: " + faculty)
+    //         return faculty;
+    //         req.query("SELECT FirstN, LastN, MiddleN, Email, Department, Balance FROM Faculty WHERE Faculty_ID=" + '\'' + faculty + '\'', function (result, recordset) {
             
-            if (recordset.recordsets[0].length > 0) { 
-                firstN = recordset.recordsets[0][0].FirstN;
-                lastN = recordset.recordsets[0][0].LastN;
-                middleN = recordset.recordsets[0][0].MiddleN;
-                fullName = firstN +' '+ lastN;
-                email = recordset.recordsets[0][0].Email;
-                dep = recordset.recordsets[0][0].Department;
-                balance = recordset.recordsets[0][0].Balance;
-                balance = balance + '.00';
-            
-                window.localStorage.setItem("faculty", ID);
-                window.localStorage.setItem("fullName", fullName);
-                window.localStorage.setItem("email", email);
-                window.localStorage.setItem("balance", balance);
-                window.localStorage.setItem("dep", dep);
-            //   document.getElementById("ID").innerHTML = faculty;
-            //   document.getElementById("fullName").innerHTML = fullName;
-            //   document.getElementById("email").innerHTML = email;
-            //   document.getElementById("balance").innerHTML = balance;
-            //   document.getElementById("dep").innerHTML = dep;
-                return;
-            }
-          })
-        }else if(stud != null){
-            return stud;
-            req.query("SELECT FirstN, LastN, MiddleN, Email, Major, Balance FROM Students WHERE StudentID=" + '\'' + stud + '\'', function (result, recordset) {
-            if (recordset.recordsets[0].length > 0) { 
-                firstN = recordset.recordsets[0][0].FirstN;
-                lastN = recordset.recordsets[0][0].LastN;
-                middleN = ' ' + recordset.recordsets[0][0].MiddleN + ' ';
-                fullName = firstN +' '+ lastN;
-                email = recordset.recordsets[0][0].Email;
-                major = recordset.recordsets[0][0].Major;
-                balance = recordset.recordsets[0][0].Balance;
-                balance = balance + '.00';
-                localStorage.setItem("stud", ID);
-                localStorage.setItem("fullName", fullName);
-                localStorage.setItem("email", email);
-                localStorage.setItem("balance", balance);
-                localStorage.setItem("major", dep);
-            //   document.getElementById("ID").innerHTML = stud;
-            //   document.getElementById("fullName").innerHTML = fullName;
-            //   document.getElementById("email").innerHTML = email;
-            //   document.getElementById("balance").innerHTML = balance;
-            //   document.getElementById("dep").innerHTML = major;
-                return;
-            }
-          })
-        }
-        else if(guest != null){
-            return guest;
-            req.query("SELECT FirstN, LastN, MiddleN, Email, Balance FROM Guest WHERE GuestID=" + '\'' + guest + '\'', function (result, recordset) {
-            // console.log(result);
-            if (recordset.recordsets[0].length > 0) { 
-                firstN = recordset.recordsets[0][0].FirstN;
-                lastN = recordset.recordsets[0][0].LastN;
-                middleN = ' ' + recordset.recordsets[0][0].MiddleN + ' ';
-                fullName = firstN +' '+ lastN;
-                email = recordset.recordsets[0][0].Email;
-                balance = recordset.recordsets[0][0].Balance;
-                balance = balance + '.00';
-                localStorage.setItem("guest", ID);
-                localStorage.setItem("fullName", fullName);
-                localStorage.setItem("email", email);
-                localStorage.setItem("balance", balance);
-                //   document.getElementById("ID").innerHTML = guest;
-                //   document.getElementById("fullName").innerHTML = fullName;
-                //   document.getElementById("email").innerHTML = email;
-                //   document.getElementById("balance").innerHTML = balance;
-                return;
-            }
-          })
-        }
-        else {
-            console.log('Error');
-            return;
-        }
-      })
-    });
-}
+    //         if (recordset.recordsets[0].length > 0) { 
+    //             firstN = recordset.recordsets[0][0].FirstN;
+    //             lastN = recordset.recordsets[0][0].LastN;
+    //             middleN = recordset.recordsets[0][0].MiddleN;
+    //             fullName = firstN +' '+ lastN;
+    //             email = recordset.recordsets[0][0].Email;
+    //             dep = recordset.recordsets[0][0].Department;
+    //             balance = recordset.recordsets[0][0].Balance;
+    //             balance = balance + '.00';
+    //             return;
+    //         }
+    //       })
+    //     }else if(stud != null){
+    //         return stud;
+    //         req.query("SELECT FirstN, LastN, MiddleN, Email, Major, Balance FROM Students WHERE StudentID=" + '\'' + stud + '\'', function (result, recordset) {
+    //         if (recordset.recordsets[0].length > 0) { 
+    //             firstN = recordset.recordsets[0][0].FirstN;
+    //             lastN = recordset.recordsets[0][0].LastN;
+    //             middleN = ' ' + recordset.recordsets[0][0].MiddleN + ' ';
+    //             fullName = firstN +' '+ lastN;
+    //             email = recordset.recordsets[0][0].Email;
+    //             major = recordset.recordsets[0][0].Major;
+    //             balance = recordset.recordsets[0][0].Balance;
+    //             balance = balance + '.00';
+    //             return;
+    //         }
+    //       })
+    //     }
+    //     else if(guest != null){
+    //         return guest;
+    //         req.query("SELECT FirstN, LastN, MiddleN, Email, Balance FROM Guest WHERE GuestID=" + '\'' + guest + '\'', function (result, recordset) {
+    //         // console.log(result);
+    //         if (recordset.recordsets[0].length > 0) { 
+    //             firstN = recordset.recordsets[0][0].FirstN;
+    //             lastN = recordset.recordsets[0][0].LastN;
+    //             middleN = ' ' + recordset.recordsets[0][0].MiddleN + ' ';
+    //             fullName = firstN +' '+ lastN;
+    //             email = recordset.recordsets[0][0].Email;
+    //             balance = recordset.recordsets[0][0].Balance;
+    //             balance = balance + '.00';
+    //             return;
+    //         }
+    //       })
+    //     }
+    //     else {
+    //         console.log('Error');
+    //         return;
+    //     }
+      }
+    //   )
+//     });
+// }
 
 function PasswordChanger(response, postData) {
     var req = new sql.Request();
